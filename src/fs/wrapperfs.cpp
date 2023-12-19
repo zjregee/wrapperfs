b81b17a0f2386356227d04442b4f88f5d63d72b1
zjregee <zjregee@gmail.com>
2023-12-19 17:41:55 +0800

    update

--- src/fs/wrapperfs.cpp
+++ src/fs/wrapperfs.cpp
@@ -7,4 +7,44 @@ wrapperfs::wrapperfs(const std::string &data_dir, const std::string &db_dir) {
     adaptor_ = new LevelDBAdaptor(db_dir);
 }
 
+int wrapperfs::getattr(const char* path, struct stat* statbuf) {
+    return 0;
+}
+
+int wrapperfs::mknod(const char* path, mode_t mode, dev_t dev) {
+    return 0;
+}
+
+int wrapperfs::mkdir(const char* path, mode_t mode) {
+    return 0;
+}
+
+int wrapperfs::open(const char* path, struct fuse_file_info* file_info) {
+    return 0;
+}
+
+int wrapperfs::read(const char* path, char* buf, size_t size, off_t offset, struct fuse_file_info* file_info) {
+    return 0;
+}
+
+int wrapperfs::write(const char* path, const char* buf, size_t size, off_t offset, struct fuse_file_info* file_info) {
+    return 0;
+}
+
+int wrapperfs::release(const char* path, struct fuse_file_info* file_info) {
+    return 0;
+}
+
+int wrapperfs::opendir(const char* path, struct fuse_file_info* file_info) {
+    return 0;
+}
+
+int wrapperfs::readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* file_info) {
+    return 0;
+}
+
+int wrapperfs::releasedir(const char* path, struct fuse_file_info* file_info) {
+    return 0;
+}
+
 }
--- tools/fs.cpp
+++ tools/fs.cpp
@@ -13,70 +13,70 @@ static int wrap_getattr(const char* path, struct stat* stat_buf) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("getattr called: path - {}", path);
     }
-    return 0;
+    return fs->getattr(path, stat_buf);
 }
 
 static int wrap_mknod(const char* path, mode_t mode, dev_t dev) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("mknod called: path - {}", path);
     }
-    return 0;
+    return fs->mknod(path, mode, dev);
 }
 
 static int wrap_mkdir(const char* path, mode_t mode) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("mkdir called: path - {}", path);
     }
-    return 0;
+    return fs->mkdir(path, mode);
 }
 
 static int wrap_open(const char* path, struct fuse_file_info* file_info) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("open called: path - {}", path);
     }
-    return 0;
+    return fs->open(path, file_info);
 }
 
 static int wrap_read(const char* path, char* buf, size_t size, off_t offset, struct fuse_file_info* file_info) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("read called: path - {}", path);
     }
-    return 0;
+    return fs->read(path, buf, size, offset, file_info);
 }
 
 static int wrap_write(const char* path, const char* buf, size_t size, off_t offset, struct fuse_file_info* file_info) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("write called: path - {}", path);
     }
-    return 0;
+    return fs->write(path, buf, size, offset, file_info);
 }
 
 static int wrap_release(const char* path, struct fuse_file_info* file_info) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("release called: path - {}", path);
     }
-    return 0;
+    return fs->release(path, file_info);
 }
 
 static int wrap_opendir(const char* path, struct fuse_file_info* file_info) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("opendir called: path - {}", path);
     }
-    return 0;
+    return fs->opendir(path, file_info);
 }
 
 static int wrap_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* file_info) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("readdir called: path - {}", path);
     }
-    return 0;
+    return fs->readdir(path, buf, filler, offset, file_info);
 }
 
 static int wrap_releasedir(const char* path, struct fuse_file_info* file_info) {
     if (wrapperfs::ENABELD_LOG) {
         spdlog::info("releasedir called: path - {}", path);
     }
-    return 0;
+    return fs->releasedir(path, file_info);
 }
 
 static struct fuse_operations operations = {
