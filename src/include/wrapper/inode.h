c04f721fd265030dc1de36a244b38c2a75209f1f
zjregee <zjregee@gmail.com>
2024-01-08 18:27:51 +0800

    update

--- src/include/wrapper/inode.h
+++ src/include/wrapper/inode.h
@@ -12,19 +12,46 @@
 
 namespace wrapperfs {
 
-struct inode_metadata {
+struct inode_metadata_t {
     struct stat stat;
+
+    std::string debug() {
+        std::stringstream s;
+        s << "inode_id - " << stat.st_ino;
+        return s.str();
+    }
 };
 
-struct inode_data {
+struct inode_data_t {
     std::unordered_map<std::string, std::string> map;
+
+    std::string debug() {
+        std::stringstream s;
+        for (auto &kv : map) {
+            s << kv.first << " - " << kv.second;
+        }
+        return s.str();
+    }
 };
 
 struct inode_t {
-    inode_metadata metadata;
-    inode_data data;
+    inode_metadata_t metadata;
+    inode_data_t data;
+
+    std::string debug() {
+        std::stringstream s;
+        s << "inode - " << metadata.stat.st_ino;
+        for (auto &kv : data.map) {
+            s << " " << kv.first << " - " << kv.second;
+        }
+        return s.str();
+    }
 };
 
+bool get_inode_metadata(LevelDBAdaptor* adaptor, size_t inode_id, inode_metadata_t* &inode_metadata);
+bool put_inode_metadata(LevelDBAdaptor* adaptor, size_t inode_id, inode_metadata_t* &inode_metadata);
+bool get_inode_data(LevelDBAdaptor* adaptor, size_t inode_id, inode_data_t* &inode_data);
+bool put_inode_data(LevelDBAdaptor* adaptor, size_t inode_id, inode_data_t* &inode_data);
 bool get_inode(LevelDBAdaptor* adaptor, size_t inode_id, inode_t* &inode);
 bool put_inode(LevelDBAdaptor* adaptor, size_t inode_id, inode_t* inode);
 
--- src/wrapper/inode.cpp
+++ src/wrapper/inode.cpp
@@ -16,65 +16,117 @@ std::string decode_inode_data(size_t inode_id) {
     return oss.str();
 }
 
-bool get_inode(LevelDBAdaptor* adaptor, size_t inode_id, inode_t* &inode) {
-    if (ENABELD_LOG) {
-        spdlog::info("get_inode: inode_id - {}", inode_id);
-    }
+bool get_inode_metadata(LevelDBAdaptor* adaptor, size_t inode_id, inode_metadata_t* &inode_metadata) {
     std::string metadata_key = decode_inode_metadata(inode_id);
-    std::string data_key = decode_inode_data(inode_id);
     std::string metadata_value;
-    std::string data_value;
     if (!adaptor->GetValue(metadata_key, metadata_value)) {
-        inode = nullptr;
-        spdlog::error("get inode error");
+        inode_metadata = nullptr;
+        spdlog::error("get inode metadata inode_id - {}: inode metadata doesn't exist");
         return false;
     }
-    if (!adaptor->GetValue(data_key, data_value)) {
-        inode = nullptr;
-        spdlog::error("get inode error");
-        return false;
-    }
-    inode = new inode_t;
-    const inode_metadata* metadata = reinterpret_cast<const inode_metadata *>(metadata_value.data());
-    std::memcpy(&(inode->metadata), metadata, sizeof(inode_metadata));
-    try {
-        nlohmann::json json_map = nlohmann::json::parse(data_value);
-        inode->data.map = json_map.get<std::unordered_map<std::string, std::string>>();
-    } catch (const std::exception& e) {
-        spdlog::error("get inode error");
-        inode = nullptr;
-        return false;
+    inode_metadata = new inode_metadata_t;
+    const inode_metadata_t* metadata = reinterpret_cast<const inode_metadata_t *>(metadata_value.data());
+    std::memcpy(&inode_metadata, metadata, sizeof(inode_metadata_t));
+    if (ENABELD_LOG) {
+        spdlog::info("get inode metadata inode_id - {}: {}", inode_id, inode_metadata->debug());
     }
     return true;
 }
 
-bool put_inode(LevelDBAdaptor* adaptor, size_t inode_id, inode_t* inode) {
-    if (ENABELD_LOG) {
-        spdlog::info("put_inode: inode_id - {}", inode_id);
-    }
-    if (inode == nullptr) {
-        spdlog::error("put inode error");
+bool put_inode_metadata(LevelDBAdaptor* adaptor, size_t inode_id, inode_metadata_t* &inode_metadata) {
+    if (inode_metadata == nullptr) {
+        spdlog::error("put inode metadata inode_id - {}: inode metadata doesn't exist");
         return false;
     }
+    if (ENABELD_LOG) {
+        spdlog::info("put inode metadata inode_id - {}: {}", inode_id, inode_metadata->debug());
+    }
     std::string metadata_key = decode_inode_metadata(inode_id);
-    std::string data_key = decode_inode_data(inode_id);
-    std::string metadata_value;
-    std::string data_value;
-    metadata_value = std::string(reinterpret_cast<const char*>(&(inode->metadata)), sizeof(inode_metadata));
-    nlohmann::json json_map = inode->data.map;
-    data_value = json_map.dump();
+    std::string metadata_value = std::string(reinterpret_cast<const char*>(&inode_metadata), sizeof(inode_metadata_t));
     if (!adaptor->Insert(metadata_key, metadata_value)) {
-        spdlog::error("put inode error");
+        spdlog::error("put inode metadata inode_id - {}: kv store interanl error", inode_id);
+        delete inode_metadata;
+        return false;
+    }
+    delete inode_metadata;
+    return true;
+}
+
+bool get_inode_data(LevelDBAdaptor* adaptor, size_t inode_id, inode_data_t* &inode_data) {
+    std::string data_key = decode_inode_data(inode_id);
+    std::string data_value;
+    if (!adaptor->GetValue(data_key, data_value)) {
+        inode_data = nullptr;
+        spdlog::error("get inode data inode_id - {}: inode data doesn't exist");
+        return false;
+    }
+    inode_data = new inode_data_t;
+    try {
+        nlohmann::json json_map = nlohmann::json::parse(data_value);
+        inode_data->map = json_map.get<std::unordered_map<std::string, std::string>>();
+    } catch (const std::exception& e) {
+        spdlog::error("get inode data inode_id -{}: unresolved data format");
+        inode_data = nullptr;
+        return false;
+    }
+    if (ENABELD_LOG) {
+        spdlog::info("get inode metadata inode_id - {}: {}", inode_id, inode_data->debug());
+    }
+    return true;
+}
+
+bool put_inode_data(LevelDBAdaptor* adaptor, size_t inode_id, inode_data_t* &inode_data) {
+    if (inode_data == nullptr) {
+        spdlog::error("put inode data inode_id - {}: inode data doesn't exist");
+        return false;
+    }
+    if (ENABELD_LOG) {
+        spdlog::info("put inode data inode_id - {}: {}", inode_id, inode_data->debug());
+    }
+    std::string data_key = decode_inode_data(inode_id);
+    nlohmann::json json_map = inode_data->map;
+    std::string data_value = json_map.dump();
+    if (!adaptor->Insert(data_key, data_value)) {
+        spdlog::error("put inode data inode_id - {}: kv store interanl error", inode_id);
+        delete inode_data;
+        return false;
+    }
+    delete inode_data;
+    return true;
+}
+
+bool get_inode(LevelDBAdaptor* adaptor, size_t inode_id, inode_t* &inode) {
+    inode_metadata_t* metadata;
+    inode_data_t* data;
+    if (!get_inode_metadata(adaptor, inode_id, metadata)) {
+        inode = nullptr;
+        return false;
+    }
+    if (!get_inode_data(adaptor, inode_id, data)) {
+        inode = nullptr;
+        return false;
+    }
+    inode = new inode_t;
+    std::memcpy(&inode->metadata, metadata, sizeof(inode_metadata_t));
+
+    return true;
+}
+
+bool put_inode(LevelDBAdaptor* adaptor, size_t inode_id, inode_t* inode) {
+    if (inode == nullptr) {
+        spdlog::error("put inode inode_id - {}: inode doesn't exist");
+        return false;
+    }
+    if (!put_inode_metadata(adaptor, inode_id, inode->metadata)) {
         delete inode;
         return false;
     }
-    if (!adaptor->Insert(data_key, data_value)) {
-        spdlog::error("put inode error");
+    if (!put_inode_data(adaptor, inode_id, inode->data)) {
         delete inode;
         return false;
     }
     delete inode;
     return true;
 }
-    
+
 }
